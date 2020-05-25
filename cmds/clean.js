8eaf1f9e369e8bbed049df6f514e1d5a235488dc
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2020-05-25 15:59:58 +0300

    clean up message

--- cmds/clean.js
+++ cmds/clean.js
@@ -3,10 +3,10 @@ module.exports.run = async (bot, message, args, db) => {
     // role check
     const accessRoles = ['Scrim Manager'];
     let canAccess = false;
-    if (message.member.roles.cache.some(r=>accessRoles.includes(r.name))){
+    if (message.member.roles.cache.some(r => accessRoles.includes(r.name))) {
         canAccess = true;
     }
-    if(!canAccess){
+    if (!canAccess) {
         message.reply("you can't use this command!");
         return;
     }
@@ -28,18 +28,18 @@ module.exports.run = async (bot, message, args, db) => {
         members = members.filter(m => !ids.includes(m.id));
         let msg = '';
         let removedCount = members.length;
-        let removedMsg = removedCount + ' member/s were removed from the server.\n\nRemoved member/s are as follows:\n\n';    
+        let removedMsg = removedCount + ' member/s were removed from the server.\n\nRemoved member/s are as follows:\n\n';
         if (members.length != 0) {
             members.forEach(m => {
-                msg = 'Hi <@' + m.id + '>, I\'m messaging you  about TeamZtone\'s Discord server which you were apart of.\n\n' +
-                    'Time to time we do a server clean-up and in doing so we remove inactive members.\n\n' +
-                    'Because you did not opt in as an active member when you joined you\'ve now been removed from the server.\n\n' +
-                    'Please note that if you believe this happened by mistake and you wish to rejoin, please use the link below:\n\n' +
-                    'https://discord.gg/PRS2dxR \n\n' +
-                    'Don\'t forget to opt in as an active member by typing ^active\n\n' +
-                    'We wish you all the best!';
-                removedMsg += m.username + '\n';
                 bot.users.fetch(m.id).then(user => {
+                    msg = 'Hi <@' + m.id + '>, I\'m messaging you about TeamZtone\'s Discord server which you were apart of.\n\n' +
+                        'Time to time we do a server clean-up and in doing so we remove inactive members.\n\n' +
+                        'Because you did not opt in as an active member when you joined you\'ve now been removed from the server.\n\n' +
+                        'Please note that if you believe this happened by mistake and you wish to rejoin, please use the link below:\n\n' +
+                        'https://discord.gg/PRS2dxR \n\n' +
+                        'Don\'t forget to opt in as an active member by typing ^active\n\n' +
+                        'We wish you all the best!';
+                    removedMsg += m.username + '\n';
                     user.send(msg).then(() => {
                         message.guild.members.cache.get(m.id).kick();
                     });
