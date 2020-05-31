7402ce915110ab0808a7662eb2ab99ef2f7e9c46
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2020-05-31 15:33:47 +0300

    changed remind message

--- cmds/remind.js
+++ cmds/remind.js
@@ -26,7 +26,9 @@ module.exports.run = async (bot, message, args, db, prefix) => {
             activeMembers.forEach(a => ids.push(a.id));
             members = members.filter(m => !ids.includes(m.id));
             let msgArr = [];
-            let msg = '';
+            let msg = "Hi. You've been tagged as you're not down as an active member of the server. " +
+            "Please don't forget to opt in as an active member  (by typing ^active) if you don't wish to " +
+            "be removed during the upcoming clean-up that <@658815505586716692> will be carrying out. Thanks.\n";
             if (members.length != 0) {
                 members.forEach(m => {
                     if (msg.length >= 1500) {
@@ -36,8 +38,8 @@ module.exports.run = async (bot, message, args, db, prefix) => {
                     msg += '<@' + m.id + '>\n';
 
                 })
-                msg += '\nHi. You\'ve been tagged as you\'re not down as an active player. Please don\'t forget to opt in as an active user in this server (by typing ' + prefix + 'active) if you don\'t' +
-                    ' want to be removed during thethat <@658815505586716692> will be carrying out. Thanks.';
+                // msg += '\nHi. You\'ve been tagged as you\'re not down as an active player. Please don\'t forget to opt in as an active user in this server (by typing ' + prefix + 'active) if you don\'t' +
+                //     ' want to be removed during thethat <@658815505586716692> will be carrying out. Thanks.';
             } else {
                 msg = 'Everyone has opted in as an active member.';
             }
